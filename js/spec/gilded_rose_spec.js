b631f2e0ec801d4fc25712aa2d8c2daa4b956f68
Sumit Saini <saini.sumit@thinksys.com>
2022-05-16 14:08:30 +0530

    added comment for test changes

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -3,7 +3,7 @@ describe("Gilded Rose", function() {
   it("should foo", function() {
     items = [ new Item("foo", 0, 0) ];
     update_quality();
-    expect(items[0].name).toEqual("foo");
+    expect(items[0].name).toEqual("foo"); // fix this test changed value from fixme to foo 
   });
 
 });
